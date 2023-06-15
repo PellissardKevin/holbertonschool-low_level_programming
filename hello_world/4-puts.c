fd52a1e8e722d9f842a7717948c57834f605c01e
Kevin P <kevin.pellissard@gmail.com>
2023-06-15 14:33:41 -0700

    Fix the comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main(void) /* function to prints a string */
+int main(void) 
 {
 char string[] = "\"Programming is like building a multilingual puzzle";
 
 puts(string);
 
 return (0);
+/**
+ * Function to print a string
+ */
 }
