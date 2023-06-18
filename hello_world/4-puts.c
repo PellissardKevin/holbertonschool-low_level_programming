464b2f84d9baa50bf916b6a2ff10c87c5cf27db8
Kevin P <kevin.pellissard@gmail.com>
2023-06-18 07:33:01 -0700

    change ligne command

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,5 +5,8 @@
 */
 int main(void)
 {
-return (puts("\"Programming is like building a multilingual puzzle"));
+  char string[] = "\"Programming is like building a multilingual puzzle":
+  puts(string);
+  
+  return (1)
 }
