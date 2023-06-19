7f1b0ed6e54653adfb1efd90954c441967b6afa6
Kevin P <kevin.pellissard@gmail.com>
2023-06-19 01:36:02 -0700

    change include line and indentation

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
 /**
  * main - print n random int and tell if is positive or not
  * Return: 0 is success
-*/
+ */
 int main(void)
 {
 	int n;
@@ -13,17 +13,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n < 0)
+	if(n < 0)
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	else if(n > 0)
 	{
-		  printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
