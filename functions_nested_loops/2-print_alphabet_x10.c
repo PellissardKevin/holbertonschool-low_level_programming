9b313b490cd5e576b2951ce630b487abfa9da0a3
Kevin P <kevin.pellissard@gmail.com>
2023-06-20 04:28:13 -0700

    programm to print 10 times the alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabetx10 - print 10 times the alphabet
+ * print_alphabet_x10 - print 10 times the alphabet
  * Return: O when success
  */
 void print_alphabet_x10(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (y = 'a'; y <= 'z'; y++)
 		{
