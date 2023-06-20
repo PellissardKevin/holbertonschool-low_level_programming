062dc451aeaa3ed4dab2c9e794fa5dd540bf48de
Kevin P <kevin.pellissard@gmail.com>
2023-06-20 14:15:35 -0700

    programm to check last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,13 @@
  */
 int print_last_digit(int n)
 {
-	int abs, last;
+	int last;
 
-	abs = (n * ((2 * n + 1 ) %2));
-	last = (abs % 10);
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
 
 	_putchar(last + '0');
 	return (last);
