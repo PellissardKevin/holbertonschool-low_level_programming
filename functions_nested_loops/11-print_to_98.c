7842d9e25585c97c1f9e590066995320a847adc0
Kevin P <kevin.pellissard@gmail.com>
2023-06-21 13:59:52 -0700

    program to count like an idiot

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,55 +7,31 @@
  */
 void print_to_98(int n)
 {
-	int last, first;
-
-	while (n < 98 || n > 98)
+	while (n != 98)
 	{
+		if (n <= 999 && n >= 100)
+		{
+			_putchar('0' + (n / 100 % 10));
+			_putchar('0' + (n / 10 % 10));
+		}
+		else if (n > 9 && n <= 99)
+			_putchar('0' + (n / 10 % 10));
+		if (n < 0)
+		{
+			_putchar('-');
+			if (n < -9)
+				_putchar('0' + ((n * -1) / 10) % 10);
+		}
+		if (n >= 0)
+		  _putchar('0' + n % 10);
+		if (n < 0)
+			_putchar('0' + ((n * -1) %10));
+		_putchar(',');
+		_putchar(' ');
 		if (n < 98)
-		{
-			last = n % 10;
-			if (n > 9)
-			{
-			first = n / 10 % 10;
-			_putchar('0' + first);
-			}
-			if (n < 0)
-			{
-				last = ((n * -1) % 10);
-				first = ((n * -1) / 10);
-				_putchar('-');
-				if (n < -9)
-				{
-					_putchar('0' + first);
-				}
-			}
-			_putchar('0' + last);
-			_putchar(',');
-			_putchar(' ');
 			n++;
-		}
-		else
-		{
-			last = (n % 10);
-			if (n > 999)
-			{
-				first = (n / 1000) % 10;
-				_putchar('0' + first);
-				first = (n / 100) % 10;
-				_putchar('0' + first);
-			}
-			if (n > 99)
-			{
-				first = (n / 100) % 10;
-				_putchar('0' + first);
-			}
-			first = (n / 10) % 10;
-			_putchar('0' + first);
-			_putchar('0' + last);
-			_putchar(',');
-			_putchar(' ');
+		if (n > 98)
 			n--;
-		}
 	}
 	_putchar('9');
 	_putchar('8');
