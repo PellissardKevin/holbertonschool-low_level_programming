9a7ca059947b024b83655759613164ab74ecdae2
Kevin P <kevin.pellissard@gmail.com>
2023-06-22 14:50:18 -0700

    programm to check uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check if char is in uppercase
+ * Return: 1 if yes, 0 if no
+ * @c: char param
+ */
+int _isupper(int c)
+{
+	int n;
+
+	if (c >= 65 && c <= 90)
+		n = 1;
+	if (c >= 97 && c <= 122)
+		n = 0;
+
+	return(n);
+}
