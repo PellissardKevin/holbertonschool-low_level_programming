0d68d245836e5704b9187932936fff7fceec9a7d
Kevin P <kevin.pellissard@gmail.com>
2023-06-23 14:14:33 -0700

    print mirror triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle with size
+ * @size: size of triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if(size <= 0)
+		_putchar('\n');
+	else
+	{
+		for(i = 0; i < size; i++)
+		{
+			for(j = 0; j < size; j++)
+			{
+				if(j < size-i-1)
+					_putchar(' ');
+				else
+					_putchar('*');
+			}
+			_putchar('\n');
+		}
+	}
+}
