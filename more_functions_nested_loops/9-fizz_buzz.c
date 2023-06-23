52bd68ff5a3a7933928e2f3db20c023adb1a5d0e
Kevin P <kevin.pellissard@gmail.com>
2023-06-23 13:42:48 -0700

    program to print fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (i = 1; i <= n; i++)
 	{
-		if ((i % 15) == 0)
+		if (((i % 15) == 0) && (i < n))
 			printf("FizzBuzz ");
 
 		else if ((i % 3) == 0)
@@ -22,6 +22,8 @@ int main(void)
 		else
 			printf("%d ", i);
 
+		if (i == n)
+			printf("FizzBuzz\n");
 	}
 
 	return (0);
