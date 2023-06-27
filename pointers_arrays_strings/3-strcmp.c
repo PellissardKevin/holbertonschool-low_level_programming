5b5e066646614c64f5249cdb1e6cf7545e39346b
Kevin P <kevin.pellissard@gmail.com>
2023-06-27 16:23:45 -0700

    fix prog

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int tot;
+	int tot, i;
 
-	tot = s1[0] - s2[0];
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			tot = s1[i] - s2[i];
+			break;
+		}
+		else
+			tot = 0;
+	}
 
 	return (tot);
 }
