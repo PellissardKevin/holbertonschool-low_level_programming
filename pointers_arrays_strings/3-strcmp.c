405db9410ef5907d31d040a7bfbd940fdf015e47
Kevin P <kevin.pellissard@gmail.com>
2023-06-27 16:00:06 -0700

    compare two string

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcmp - compare two string
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int tot;
+
+	tot = s1[0] - s2[0];
+
+	return (tot);
+}
