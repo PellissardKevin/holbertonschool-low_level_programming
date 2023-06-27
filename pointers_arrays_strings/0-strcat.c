3d0404d37e921ad9a30d269342d746a8ec2810d5
Kevin P <kevin.pellissard@gmail.com>
2023-06-27 15:30:35 -0700

    Cat a string with an other

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - copy and cat a two string
+ * @dest: string to receve cat
+ * @src: source for cat
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while(*p != '\0')
+	 	p++;
+
+	while(*src != '\0')
+	{
+		*p++ = *src++;
+	}
+
+	*p = '\0';
+
+	return dest;
+}
