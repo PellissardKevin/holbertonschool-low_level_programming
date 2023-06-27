5e549cfa0929fceaa6c8b710d924686ac77aa430
Kevin P <kevin.pellissard@gmail.com>
2023-06-27 16:35:38 -0700

    reverse integer from an array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverse content of an array
+ * @a: array to reverse
+ * @n: number of elements
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n -1];
+		a[n -1] = tmp;
+		n--;
+	}
+}
