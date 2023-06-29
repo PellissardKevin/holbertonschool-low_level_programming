d838f08c516a767ddc88e76be4c5370e1df73df9
Kevin P <kevin.pellissard@gmail.com>
2023-06-29 07:58:10 -0700

    set memory on an array

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - change memory with constant byte
+ * @s: pointer string
+ * @b: constant byte
+ * @n: n bytes of memory pointed
+ * Return: pointer to the memory s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j = 0;
+
+	while (j < n)
+	{
+		*s++ = b;
+		j++;
+	}
+
+	return (s);
+}
