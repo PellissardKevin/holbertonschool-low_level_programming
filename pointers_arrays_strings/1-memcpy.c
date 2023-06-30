587a8e872d1055aad1d8021ce745c81d87bf7ed8
Kevin P <kevin.pellissard@gmail.com>
2023-06-30 00:51:39 -0700

    Fix for UGLY BETTY

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - change memory with constant byte
- * @s: pointer string
- * @b: constant byte
+ * _memcpy - change memory with constant byte
+ * @dest: pointer string destination
+ * @src: pointer string source
  * @n: n bytes of memory pointed
- * Return: pointer to the memory s
+ * Return: pointer to the memory dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
