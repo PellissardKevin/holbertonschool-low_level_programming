1c5c89460a4ad9868e639638e219cf4c78ac7e47
Kevin P <kevin.pellissard@gmail.com>
2023-06-30 13:02:18 -0700

    search substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string source to occurate
+ * @needle: string to find in string source
+ * Return: string find
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0, first;
+	char *p = haystack;
+
+	while (p[i] != '\0')
+	{
+		while (p[i] != needle[0] && p[i] != '\0')
+			i++;
+
+		if (p[i] == '\0')
+			return (0);
+
+		first = i;
+
+		while (p[i] == needle[j] && p[i] != '\0' && needle[j] != '\0')
+		{
+			i++;
+			j++;
+		}
+
+		if (needle[j] == '\0')
+			return (p + first);
+		if (p[i] == '\0')
+			return (0);
+
+		i = first + 1;
+		j = 0;
+	}
+
+	return (p);
+}
