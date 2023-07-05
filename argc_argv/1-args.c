ce49ae4773b455c6ff4aae49ebabad62a67e52db
Kevin P <kevin.pellissard@gmail.com>
2023-07-05 02:58:03 -0700

    program print all arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define UNUSED(argv) (void)(argv)
+/**
+ * main - print all arguments
+ * @argc: int
+ * @argv: list arguments
+ * Return: 0 success
+ */
+int main(int argc, char * argv[])
+{
+	UNUSED (argv);
+	printf("%d\n", (argc - 1));
+
+	return (0);
+}
