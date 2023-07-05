c650ff6347e5c13b97dc99c0240afdc6cd5aef72
Kevin P <kevin.pellissard@gmail.com>
2023-07-05 01:30:46 -0700

    programm print is own name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print name's program
+ * @argc: int argc
+ * @argv: array of list
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
+	{
+		printf("%s ", argv[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
