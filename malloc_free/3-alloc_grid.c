5e2c87ebce607ec0916e2eac82858ca581a03b02
Kevin P <kevin.pellissard@gmail.com>
2023-07-10 06:18:21 -0700

    print an array of integer

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocate memorory a grid of integer
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: pointer to a 2d array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int x, y, z, l;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = malloc(height);
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		ptr[x] = malloc(width);
+
+		if (ptr[x] == NULL)
+		{
+			for (y = x; y >= 0; y--)
+				free(ptr[y]);
+			free(ptr);
+			return (NULL);
+		}
+	}
+
+	for (z = 0; z < height; z++)
+	{
+		for (l = 0; l < width; l++)
+			ptr[z][l] = 0;
+	}
+
+	return (ptr);
+}
