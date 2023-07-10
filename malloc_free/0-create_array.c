9c7f83e67b1f34d728e80bc192b4f3cdde14bb83
Kevin P <kevin.pellissard@gmail.com>
2023-07-10 01:54:22 -0700

    Fix for Ugly Betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,10 +15,10 @@ char *create_array(unsigned int size, char c)
 	array = (char *)malloc(sizeof(char) * (size + 1));
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 
-	if (!(array = calloc(size, sizeof(char))))
-		return NULL;
+	if (!(calloc(size, sizeof(char))))
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
