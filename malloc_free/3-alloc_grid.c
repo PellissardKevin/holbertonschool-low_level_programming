7effa391248ed981d49d0e93b639cffaf7a4b56b
Kevin P <kevin.pellissard@gmail.com>
2023-07-10 06:22:13 -0700

    print an array of integer

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * alloc_grid - allocate memorory a grid of integer
  * @width: width of the grid
@@ -12,12 +11,12 @@
 int **alloc_grid(int width, int height)
 {
 	int **ptr;
-	int x, y, z, l;
+	int x, y, z;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = malloc(height);
+	ptr = malloc(sizeof(int) * height);
 
 	if (ptr == NULL)
 	{
@@ -27,21 +26,19 @@ int **alloc_grid(int width, int height)
 
 	for (x = 0; x < height; x++)
 	{
-		ptr[x] = malloc(width);
+		ptr[x] = malloc(sizeof(int) * width);
 
 		if (ptr[x] == NULL)
 		{
-			for (y = x; y >= 0; y--)
-				free(ptr[y]);
 			free(ptr);
 			return (NULL);
 		}
 	}
 
-	for (z = 0; z < height; z++)
+	for (y = 0; y < height; y++)
 	{
-		for (l = 0; l < width; l++)
-			ptr[z][l] = 0;
+		for (z = 0; z < width; z++)
+			ptr[y][z] = 0;
 	}
 
 	return (ptr);
