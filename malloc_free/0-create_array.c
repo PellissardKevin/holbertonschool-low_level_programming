614649ce00c55e910f96131e776bdd740a2fceaf
Kevin P <kevin.pellissard@gmail.com>
2023-07-10 02:09:21 -0700

    Fix4

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (!(calloc(size, sizeof(char))))
 		return (NULL);
 
-	array = calloc(size, sizeof(char));
+	array = calloc(size - 1, sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
