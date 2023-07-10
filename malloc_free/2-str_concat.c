87d945023f21da35e9ae6e70544002c5f690db7d
Kevin P <kevin.pellissard@gmail.com>
2023-07-10 05:01:06 -0700

    concat string with malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	/* if s1 = NULL or s2 = NULL */
 	if (s1 == NULL)
 		s1 = '\0';
-	if (s2 == NULL)
+	else if (s2 == NULL)
 		s2 = '\0';
 
 	/*  size of string s1 and s2 */
