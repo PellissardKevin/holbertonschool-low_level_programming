38414656e2265eac04bd3c6b4f85f06fa28de4a9
Kevin P <kevin.pellissard@gmail.com>
2023-07-11 06:27:45 -0700

    fix the code

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * array_range - create an array of integer
+ * @min: min of array
+ * @max: maximum of the range
+ *
+ * Return: return an array
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	
+}
