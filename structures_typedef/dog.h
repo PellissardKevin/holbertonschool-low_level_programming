48b8c52fb50c9d2898b46c5075eed9aa3ee818e2
Kevin P <kevin.pellissard@gmail.com>
2023-07-17 02:37:10 -0700

    new type dog_t to type the struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: proprio
  * Description: Fiche d'un chien que je ne connais pas
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
