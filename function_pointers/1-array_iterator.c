ed85aa85d1cb689e4ce6bdbc56e47c39ff7b1ed0
Kevin P <kevin.pellissard@gmail.com>
2023-07-17 07:12:42 -0700

    fix issue from null param

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * print_elem - prints an integer
+ * array_iterator - array of integer from an function
  * @array: array of integer
  * @size: size of array
  * @action: pointer to a function
@@ -11,6 +11,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
+	if (array == NULL || action == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		action(array[i]);
