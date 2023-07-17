c64f69d41285080dcd4e2643d8ee1d01cbefd95c
Kevin P <kevin.pellissard@gmail.com>
2023-07-17 01:21:15 -0700

    fix for Ugly Betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef _struct
 #define _struct
-(struct dog
+/**
+ * struct dog - structure of dog
+ * @name: nom
+ * @age: age of dog
+ * @owner: proprio
+ * Description: Fiche d'un chien que je ne connais pas
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} Dog;)
+};
 #endif
