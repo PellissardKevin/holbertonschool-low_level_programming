3d02489b4143ef07f65ecb13f6cc6264f6ef26a1
Kevin.P <kevin.pellissard@gmail.com>
2023-07-18 15:48:45 +0200

    Add function to get operation from function

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef GUARD_H
-#define GUARD_H
+#ifndef 3_CALC_H
+#define 3_CALC_H
 /**
  * struct op - Struct op
  *
@@ -17,4 +17,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-#endif
+#endif /* 3_CALC_H */
