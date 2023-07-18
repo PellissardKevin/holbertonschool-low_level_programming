eda19490659ae5824f086ecb1334246b624df5be
Kevin P <kevin.pellissard@gmail.com>
2023-07-18 05:30:40 -0700

    add some file

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_functuion - select the correct function
+ * @s: string of get p function
+ * Return: Integer and function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - addition of 2 integer
+ * @a: first integer
+ * @b: second integer
+ * Return: Addition of 2 integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Substraction of 2 integer
+ * @a: first integer
+ * @b: second integer
+ * Return: Substraction of 2 integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - multiplication of 2 integer
+  * @a: first integer
+  * @b: second integer
+  * Return: multiplication of 2 integer
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of 2 integer
+ * @a: first integer
+ * @b: second integer
+ * Return: Division of 2 integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_add - modulo of 2 integer
+ * @a: first integer
+ * @b: second integer
+ * Return: modulo of 2 integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
