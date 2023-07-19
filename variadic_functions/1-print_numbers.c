fe6ab97851dec7702d2e01c4c0955842c89ce478
Kevin.P <kevin.pellissard@gmail.com>
2023-07-19 13:31:20 +0200

    fix n = null

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL)
+		if (separator != NULL && n != NULL)
 		{
 			printf("%d", va_arg(ap, int));
 		}
