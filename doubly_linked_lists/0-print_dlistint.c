e41ae67e642ab0586245bb916c826e68bb9b93da
Kevin.P <kevin.pellissard@gmail.com>
2023-08-01 11:11:31 +0200

    Add double linked list, readme, lists.h

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_dlistint - print a double linked list
+ * @h: elements of list
+ * Return: number of node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int size = 0;
+	const dlistint_t *p = h;
+
+	while (p != NULL)
+	{
+		if (p->n == 0)
+		{
+			printf("[0] (nil)\n");
+			p = p->next;
+			size++;
+		}
+		else
+		{
+			printf("[%d]\n", p->n);
+			p = p->next;
+			size++;
+		}
+	}
+
+	return (size);
+}
