88f9590649b6aa82a0e2cb91e97c3e6d3af503b6
Kevin.P <kevin.pellissard@gmail.com>
2023-08-01 16:03:28 +0200

    Fix for task 2 and try the check

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,15 +17,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (0);
 
 	new_node->n = n;
-	new_node->next = (*head);
+	new_node->next = *head;
 	new_node->prev = NULL;
 
 
-	if ((*head) != NULL)
+	if (*head != NULL)
 		(*head)->prev = new_node;
 
 
-	(*head) = new_node;
+	*head = new_node;
 
 	return (new_node);
 }
