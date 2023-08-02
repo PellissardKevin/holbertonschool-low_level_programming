b1e1142ecbf07bfa70e1320a1e507a7fa7018cba
Kevin.P <kevin.pellissard@gmail.com>
2023-08-02 16:49:15 +0200

    Fix for prev

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -31,9 +31,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 
 	displaynode = tmp->next;
+	displaynode->prev = tmp;
 
-	tmp->next->prev = tmp;
 	tmp->next = tmp->next->next;
+	tmp->prev = displaynode->prev;
 
 	displaynode->next = NULL;
 	free(displaynode);
