3e3c56d363d1109b3f5c4869ebc4118f3a4c87de
Kevin.P <kevin.pellissard@gmail.com>
2023-08-02 23:21:51 +0200

    fix for prev

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -18,32 +18,24 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		(*head) = (*head)->next;
 		(*head)->prev = NULL;
-		free((*head));
+		free(tmp);
 		return (1);
 	}
-	else
+
+	for (i = 0; i < index; i++)
 	{
-		for (i = 0; i < index - 1; i++)
-		{
-			if (tmp != NULL)
-				tmp = tmp->next;
-			else
-				return (-1);
-		}
-
-		if (tmp->next != NULL && tmp->prev != NULL)
-		{
-			tmp->next->prev = tmp->prev;
-			tmp->prev->next = tmp->next;
-		}
-
-		if (tmp->next == NULL)
-			tmp->prev->next = NULL;
-
-		if (tmp->prev == NULL)
-			tmp->next->prev = NULL;
-
+		if (tmp != NULL)
+			tmp = tmp->next;
+		else
+			return (-1);
 	}
+
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev;
+
+	if (tmp->prev != NULL)
+		tmp->prev->next = tmp->next;
+
 
 	return (1);
 }
