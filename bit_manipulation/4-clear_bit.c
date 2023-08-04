eeb17f0dc62d5b32589ccf085529456edc734c88
Kevin.P <kevin.pellissard@gmail.com>
2023-08-04 15:46:30 +0200

    Fix for the 4th task

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,8 +9,8 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (!n || index > 63)
-	return (-1);
+		return (-1);
 
-	*n |= (1 << index);
-	return (0);
+	*n &= ~(1 << index);
+		return (1);
 }
