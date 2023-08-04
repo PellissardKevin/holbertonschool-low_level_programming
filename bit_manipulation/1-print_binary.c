ed17e1edeae8e7bd22351baff10ef5eb4cff6659
Kevin.P <kevin.pellissard@gmail.com>
2023-08-04 11:31:12 +0200

    Convert decimal to binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * @print_binary - print number in binary
+ *
+ * @n: number to print
+ * Return: Void
+ */
+void print_binary(unsigned long int n)
+{
+
+	if (n == 0)
+		_putchar('0');
+
+	while (n > 0)
+	{
+		if (n & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		n>>=1;
+	}
+}
