80366392f3e19b4be080dc28450b3d9e3fa9e285
Kevin.P <kevin.pellissard@gmail.com>
2023-08-04 13:11:41 +0200

    Fix

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -7,19 +7,20 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i = 31, mask = (1 << i);
+	int i = 63, mask = (1 << i);
 
 	for (; (n >> i) == 0 && (i > 0); i--)
 		mask = (1 << i);
 
+printf("i = %d\n", i);
 
 	for (; i >= 0; i--)
 	{
 		mask = (1 << i);
-
 		if (n & mask)
 			_putchar('1');
 		else
 			_putchar('0');
+
 	}
 }
