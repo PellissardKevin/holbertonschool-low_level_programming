ccdf37fede37fb204f5afbdc59dab26d099cc413
Kevin.P <kevin.pellissard@gmail.com>
2023-08-07 16:44:57 +0200

    Fix for Ugly BEtty

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -2,9 +2,9 @@
 /**
  * append_text_to_file - appends text at the end of file
  *
- * @filename:
- * @text_content:
- * Retur: 1 Success
+ * @filename: name of file
+ * @text_content: text to append
+ * Return: 1 Success
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
