a0fa296c8166fba63bca7669c187ba6a19149085
Kevin.P <kevin.pellissard@gmail.com>
2023-08-08 20:38:43 +0200

    fix the loop for compare

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,13 +10,28 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index;
+	hash_node_t *temp = NULL;
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
 	if (ht == NULL || key == NULL || new_node == NULL)
 		return (0);
 
-	index = key_index((unsigned char *)key, ht->size);
+	if (value == NULL)
+		value = "";
 
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (!strcmp(temp->key, key))
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
 	new_node->key = strdup((char *)key);
 	if (!new_node->key)
 	{
