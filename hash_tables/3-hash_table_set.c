0166779aba2a5a44f01d7de9f9a216d74e6e3247
Kevin.P <kevin.pellissard@gmail.com>
2023-08-08 17:39:26 +0200

    Malloc fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,20 +9,31 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int index = key_index((unsigned char *)key, ht->size);
+	int index;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL)
 		return (0);
 
+	if (key == NULL)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	
 	if (value == NULL)
 		value = " ";
 
 	ht->array[index] = malloc(sizeof(hash_node_t));
-	if (ht->array[index] == NULL)
+	if (!ht->array[index])
 		return (0);
 
 	ht->array[index]->key = (char *)key;
 	ht->array[index]->value = strdup((char *)value);
+	if (!ht->array[index]->value)
+	{
+		free(ht->array[index]);
+		free(ht);
+		return (0);
+	}
 	ht->array[index]->next = NULL;
 
 	return (1);
