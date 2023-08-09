d969919d75d45766e454c5950951224e73e0d541
Kevin.P <kevin.pellissard@gmail.com>
2023-08-09 10:55:37 +0200

    Task 5 P2: Print a Hash Table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print a hash table
+ *
+ * @ht: Hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *newnode = NULL;
+	long unsigned int i = 0;
+
+	if (!ht)
+		return;
+
+	while (i < ht->size)
+	{
+		if (ht->array[i])
+		{
+			newnode = ht->array[i];
+			break;
+		}
+		i++;
+	}
+
+	printf("{");
+	if (newnode)
+	{
+		printf("'%s': '%s'", newnode->key, newnode->value);
+		newnode = newnode->next;
+		while (newnode)
+		{
+			printf(", '%s': '%s'", newnode->key, newnode->value);
+			newnode = newnode->next;
+		}
+		i++;
+		while (i < ht->size)
+		{
+			newnode = ht->array[i];
+			while (newnode)
+			{
+				printf(", '%s': '%s'", newnode->key, newnode->value);
+				newnode = newnode->next;
+			}
+			i++;
+		}
+	}
+	printf("}\n");
+}
